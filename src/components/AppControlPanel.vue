eee5cca828fd81d6ec103bfe06b2581bce2cdde4
Алексей <homeoflucifer@gmail.com>
2020-06-06 16:09:05 +0700

    control panel setup

---
+++ src/components/AppControlPanel.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+    div.control-panel
+        div.control-panel__full(v-if="isNewGameStarted")
+            p.control-panel__btn.control-panel__btn--attack(@click="attack") ATTACK
+            p.control-panel__btn.control-panel__btn--special-attack(@click="specialAttack") SPECIAL ATTACK
+            p.control-panel__btn.control-panel__btn--heal(@click="heal") HEAL
+            p.control-panel__btn.control-panel__btn--give-up(@click="giveUp") GIVE UP
+        div.control-panel__init(v-else)
+            p.control-panel__btn.control-panel__btn--new-game(@click="startNewGame") START NEW GAME
+</template>
+
+<script>
+import { bus } from '../main'
+
+export default {
+    name: 'AppControlPanel',
+
+    data() {
+        return {
+            isNewGameStarted: false,
+        }
+    },
+
+    methods: {
+        startNewGame() {
+            this.isNewGameStarted = true
+        },
+        attack() {
+            bus.dealDamage()
+        },
+        specialAttack() {
+            bus.dealSpecialDamage()
+        },
+        heal() {
+            bus.heal()
+        },
+        giveUp() {
+            this.isNewGameStarted = false
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .control-panel
+        &__full
+            display flex
+            justify-content space-around
+
+        &__init
+            display flex
+            justify-content center
+
+        &__btn
+            cursor pointer
+            &--attack
+                background-color red
+
+            &--special-attack
+                background-color blue
+
+            &--heal
+                background-color green
+
+            &--give-up
+                background-color yellow
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+export const bus = new Vue({
+  data: {
+    userHP: 100,
+    monsterHP: 100,
+    attackDamage: 10,
+    specialAttackDamage: 20,
+    healAmount: 8,
+    monsterAttackDamage: 7
+  },
+
+  methods: {
+    takeDamage() {
+      this.userHP -= this.monsterAttackDamage
+    },
+    heal() {
+      this.userHP += this.healAmount
+      console.log(this.userHP)
+    },
+    dealDamage() {
+      this.monsterHP -= this.attackDamage
+    },
+    dealSpecialDamage() {
+      this.monsterHP -= this.specialAttackDamage
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
